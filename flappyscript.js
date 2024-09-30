c5ee5f094ec2f260f7f0e95936955dcba5dcc6e1
Divv47 <divyanshvairagade@gmail.com>
2024-10-01 01:29:46 +0530

    fixed

--- flappyscript.js
+++ flappyscript.js
@@ -1,7 +1,13 @@
+//Board
 let board;
 let boardWidth = 360;
 let boardHeight = 640;
 let context;
+
+//bird
+let birdWidth = 34;
+let birdHeigth = 24;
+
 
 window.onload = function(){
     board = document.getElementById("board");
